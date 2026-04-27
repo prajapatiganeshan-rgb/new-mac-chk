94af3c628a60086a613f9f351231c25863ac8132
prajapatiganeshan-rgb <prajapatiganeshan@gmail.com>
2026-04-28 02:32:14 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,15 +1,4 @@
 window.defaultNumber = '+1(808) 302-9453';
 window.defaultText='Your Apple ID was recently used at APPLE STORE for $899.99 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security.Not you? Immediately call Apple Support +1-808-302-9453  to Freeze it!';
 window.text ={
-	'xhamster.com':'Your |%model%| has been locked due to detected illegal activity .Your apple id has been disabled . on |%ref%|! Immediately call Apple Support +1‪(808) 302-9453‬ to unlock it!',
-	'perfectgirls.net':'Your |%model%| has been locked due to detected illegal activity .Your apple id has been disabled . on |%ref%|! Immediately call Apple Support +1‪(808) 302-9453‬ to unlock it!',
-	'gotporn.com':'Your |%model%| has been locked due to detected illegal activity .Your apple id has been disabled . on |%ref%|! Immediately call Apple Support +1‪(808) 302-9453‬ to unlock it!',
-	'anysex.com':'Your |%model%| has been locked due to detected illegal activity .Your apple id has been disabled . on |%ref%|! Immediately call Apple Support +1‪(808) 302-9453‬ to unlock it!',
-	'sex.com':'Your |%model%| has been locked due to detected illegal activity .Your apple id has been disabled . on |%ref%|! Immediately call Apple Support +1‪(808) 302-9453‬ to unlock it!',
-	'bravotube.net':'Your |%model%| has been locked due to detected illegal activity .Your apple id has been disabled . on |%ref%|! Immediately call Apple Support +1‪(808) 302-9453‬ to unlock it!',
-	'mylust.com':'Your |%model%| has been locked due to detected illegal activity .Your apple id has been disabled . on |%ref%|! Immediately call Apple Support +1‪(808) 302-9453‬ to unlock it!',
-	'manporn.xxx':'Your |%model%| has been locked due to detected illegal activity .Your apple id has been disabled . on |%ref%|! Immediately call Apple Support +1‪(808) 302-9453‬ to unlock it!',
-	'anybunny.com':'Your |%model%| has been locked due to detected illegal activity .Your apple id has been disabled . on |%ref%|! Immediately call Apple Support +1‪(808) 302-9453‬ to unlock it!',
-	'txxx.com':'Your |%model%| has been locked due to detected illegal activity .Your apple id has been disabled . on |%ref%|! Immediately call Apple Support +1‪(808) 302-9453‬ to unlock it!',
-	'findbestsolution.xyz':'Your |%model%| has been locked due to detected illegal activity .Your apple id has been disabled . on |%ref%|! Immediately call Apple Support +1‪(808) 302-9453‬ to unlock it!'
 };
